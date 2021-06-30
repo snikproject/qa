c89f7979b324c3f912722a20ab85651b1ffe3ed0
Konrad Höffner <konrad.hoeffner@uni-leipzig.de>
2021-06-30 16:05:10 +0200

    First prototype.

--- src/eu/snik/qa/Main.java
+++ src/eu/snik/qa/Main.java
@@ -1,8 +1,10 @@
 package eu.snik.qa;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Locale;
@@ -13,6 +15,8 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.simmetrics.StringMetric;
 import org.simmetrics.builders.StringMetricBuilder;
 import org.simmetrics.metrics.DamerauLevenshtein;
@@ -21,7 +25,7 @@ import org.simmetrics.simplifiers.Simplifiers;
 public class Main {
 
 	static final String[] templates = {"What is <Class>?","What are <Class>s?",
-	"Who is <Predicate> <Object>?", "Who is <Class>?"};
+	"Who <RolePredicate> <RoleObject>?", "Who is <Class>?","What <Predicate> <Object>"};
 
 	static record Triple(String subject,String predicate, String object) {}
 	
@@ -47,6 +51,17 @@ public class Main {
 			}
 		}
 
+		Set<Triple> triples = new HashSet<Triple>();
+		Set<String> subjects = roleTriples.stream().map(t->t.subject).collect(Collectors.toSet());
+		try(Reader in = new FileReader("triples.csv"))
+		{
+			Iterable<CSVRecord> records = CSVFormat.DEFAULT.parse(in);
+			for(var record: records)
+			{
+				triples.add(new Triple(record.get(0),record.get(1),record.get(2)));
+			}
+		}
+		
 		Map<String,String> answers = new HashMap<>(); 
 		for(final String template: templates)
 		{
@@ -58,17 +73,40 @@ public class Main {
 				}
 			}
 
-			if(template.contains("<Predicate>")&&template.contains("<Object>"))
+			if(template.contains("<RolePredicate>")&&template.contains("<RoleObject>"))
 			{
 				for(Triple triple: roleTriples)
+				{
+					answers.put(template.replace("<RolePredicate>", triple.predicate).replace("<RoleObject>", triple.object), triple.subject);
+				}
+			}
+
+			if(template.contains("<Predicate>")&&template.contains("<Object>"))
+			{
+				for(Triple triple: triples)
 				{
 					answers.put(template.replace("<Predicate>", triple.predicate).replace("<Object>", triple.object), triple.subject);
 				}
 			}
-			
+
 		}
 
+		
+		try(Writer out = new FileWriter("answers.json"))
+		{			
+			JSONArray json = new JSONArray();
+			for(var entry: answers.entrySet())
+			{
+				JSONObject obj = new JSONObject();
+				obj.put("question",entry.getKey());
+				obj.put("answer",entry.getValue());
+				json.put(obj);
+			}
+			out.write(json.toString());
+		}
 		//answers.forEach((a,b) -> {System.out.println(a+"  Answer: "+b);});
+		
+		
 		Scanner input = new Scanner(System.in);
 		StringMetric metric = StringMetricBuilder.with(new DamerauLevenshtein())
 				.simplify(Simplifiers.toLowerCase(Locale.ENGLISH))
@@ -94,7 +132,10 @@ public class Main {
 			}
 
 			System.out.println("Answer: "+answers.get(best)+" "+max);
+			
 		}
+		
+		
 	}
 
 }
